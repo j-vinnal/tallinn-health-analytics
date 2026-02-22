d27bc28fa6976596f897518fdcd91835cdb43e70
Jüri Vinnal <juvinn@taltech.ee>
2026-02-22 05:24:39 +0200

    add: source ETU41

--- src/steps/api_to_raw.py
+++ src/steps/api_to_raw.py
@@ -19,9 +19,11 @@ def run(ctx) -> None:
 def _fetch(source_id: str, api_path: str, api_cfg: dict, output_root: Path) -> Path:
     output_dir = output_root / source_id
     output_dir.mkdir(parents=True, exist_ok=True)
-    for f in output_dir.glob("*"):
-        if f.is_file():
-            f.unlink()
+
+# Peab mõtlema, kas see on vajalik
+#    for f in output_dir.glob("*"):
+#        if f.is_file():
+#            f.unlink()
 
     url = f"{api_cfg['api_root']}/{api_path}"
     resp = requests.post(
@@ -32,6 +34,6 @@ def _fetch(source_id: str, api_path: str, api_cfg: dict, output_root: Path) -> P
     resp.raise_for_status()
 
     ts = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = output_dir / f"{source_id}_{ts}.csv"
+    file_path = output_dir / f"{source_id}_{ts}.json"
     file_path.write_bytes(resp.content)
     return file_path
